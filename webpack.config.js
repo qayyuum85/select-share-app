a9953315f5f391491b7214a2702df3d95f66e199
Qayyuum Harun <qayyuum@seekasia.com>
2021-01-24 10:54:06 +0800

    Add source map

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const Dotenv = require('dotenv-webpack');
 module.exports = {
   mode: 'development',
   entry: './src/app.ts',
+  devtool: 'inline-source-map',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
